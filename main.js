6f5630a2b9d48808b776e6bc44aa3b5e4ed4f024
serg <petrov@gmail.com>
2023-05-17 18:52:12 +0300

    thirds commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','') ;
+
+const personalMovieDB = {
+    count : personalMovieDB,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+
+ const a = prompt ('Один из последних просмотренных фильмов?', ''),
+        b  = prompt('На сколько оцените его?', ''),
+        c = prompt ('Один из последних просмотренных фильмов?', ''),
+        d  = prompt('На сколько оцените его?', '');
+        
+numberOfFilms.movies[a] = b;
+numberOfFilms.movies[c] = d;
