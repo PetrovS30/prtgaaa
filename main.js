17a4af97ac429e4627a56179f6a88643bb5e844a
serg <petrov@gmail.com>
2023-05-20 16:17:53 +0300

    gottt

--- main.js
+++ main.js
@@ -1,17 +1,7 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','') ;
+const str = "АННА" ;
+ const test = str.split("  ").reverse().join("") 
+    if (test === "АННА")  
+        console.log(test);
+    
 
-const personalMovieDB = {
-    count : personalMovieDB,
-    movies : {},
-    actors : {},
-    genres : [],
-    privat : false
-};
-
- const a = prompt ('Один из последних просмотренных фильмов?', ''),
-        b  = prompt('На сколько оцените его?', ''),
-        c = prompt ('Один из последних просмотренных фильмов?', ''),
-        d  = prompt('На сколько оцените его?', '');
-        
-numberOfFilms.movies[a] = b;
-numberOfFilms.movies[c] = d;
+ 
